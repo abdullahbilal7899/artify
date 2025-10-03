fa7d3d40fe7406cc92c927521338985b7f1ec52b
abdullahbilal7899 <107762569+abdullahbilal7899@users.noreply.github.com>
2025-10-03 11:37:49 +0500

    Update App.jsx

--- artify/client/src/App.jsx
+++ artify/client/src/App.jsx
@@ -24,7 +24,7 @@ Create
    <main className='sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]'>
 <Routes>
   <Route path='/' element={<Home/>}/>
-  <Route path='/create-post' element={<CreatePost/>}/>
+  <Route path='/create' element={<CreatePost/>}/>
 </Routes>
    </main>
    <ToastContainer/>
