d354bf10ae33c53074e12ef2f578487312183720
abdullahbilal7899 <107762569+abdullahbilal7899@users.noreply.github.com>
2025-10-03 10:56:45 +0500

    Create api.js

---
+++ artify/client/src/api.js
@@ -0,0 +1,6 @@
+const BASE_URL = "https://artify-backend-one.vercel.app"; // put your actual vercel backend link here
+
+export const API = {
+  post: `${BASE_URL}/api/v1/post`,
+  dalle: `${BASE_URL}/api/v1/dalle`
+};
